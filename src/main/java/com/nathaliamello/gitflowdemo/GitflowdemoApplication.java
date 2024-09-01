69943de5e4930ca9a5583066513d80672f1bbefd
Nathalia Mello <nathaliamello.ntm@gmail.com>
2024-09-01 19:39:59 -0300

    Initial commit

---
+++ src/main/java/com/nathaliamello/gitflowdemo/GitflowdemoApplication.java
@@ -0,0 +1,13 @@
+package com.nathaliamello.gitflowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitflowdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nathaliamello/gitflowdemo/GitflowdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathaliamello.gitflowdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
